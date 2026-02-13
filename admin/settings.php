a8c3e9bd4400d6e5adb571ac97441accbcb936a5
Djnirds1984 <aldrincabanez9@gmail.com>
2026-02-13 17:26:43 +0800

    Remove redundant hotspot settings from general settings page

--- admin/settings.php
+++ admin/settings.php
@@ -7,28 +7,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $coin_val = escapeshellarg($_POST['coin_value']);
     $time_val = escapeshellarg($_POST['time_per_coin']);
     $portal_url = escapeshellarg($_POST['portal_url']);
-    $ssid = escapeshellarg($_POST['ssid']);
     
     // System Settings
     exec("uci set $configFile.general.coin_value=$coin_val");
     exec("uci set $configFile.general.time_per_coin=$time_val");
     exec("uci set $configFile.general.portal_url=$portal_url");
     
-    // Hotspot/Wireless Settings
-    // Find the first AP interface to update SSID
-    exec("uci show wireless | grep '=wifi-iface' | grep '.mode='ap'' | head -n 1 | cut -d. -f2 | cut -d= -f1", $iface_output);
-    if (isset($iface_output[0]) && $iface_output[0]) {
-        $iface = $iface_output[0];
-        exec("uci set wireless.$iface.ssid=$ssid");
-    }
-    
     exec("uci commit $configFile");
-    exec("uci commit wireless");
     
-    // Reload services
-    exec("wifi reload");
+    // Reload services (optional if backend polls)
+    // exec("wifi reload"); // Not needed if we don't touch wireless here
     
-    $msg = "Settings saved successfully. WiFi may restart.";
+    $msg = "Settings saved successfully.";
 }
 
 // Read current config
@@ -36,13 +26,9 @@ exec("uci get $configFile.general.coin_value 2>/dev/null", $o1);
 exec("uci get $configFile.general.time_per_coin 2>/dev/null", $o2);
 exec("uci get $configFile.general.portal_url 2>/dev/null", $o3);
 
-// Get current SSID
-exec("uci show wireless | grep '.ssid=' | head -n 1 | cut -d= -f2 | tr -d \"'\"", $o4);
-
 $current_coin = isset($o1[0]) ? $o1[0] : '1';
 $current_time = isset($o2[0]) ? $o2[0] : '10';
 $current_url = isset($o3[0]) ? $o3[0] : 'http://10.0.0.1/pisowifi';
-$current_ssid = isset($o4[0]) ? $o4[0] : 'Pisowifi';
 ?>
 
 <div class="card">
@@ -59,10 +45,7 @@ $current_ssid = isset($o4[0]) ? $o4[0] : 'Pisowifi';
         <label>Time per Coin (Minutes)</label>
         <input type="number" name="time_per_coin" value="<?php echo $current_time; ?>" required>
         
-        <h4>Hotspot Settings</h4>
-        <label>WiFi Name (SSID)</label>
-        <input type="text" name="ssid" value="<?php echo $current_ssid; ?>" required>
-        
+        <h4>Portal Settings</h4>
         <label>Portal URL (Redirect)</label>
         <input type="text" name="portal_url" value="<?php echo $current_url; ?>" required>
         
